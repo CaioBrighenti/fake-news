0958ac886af1d758543e248b16f6a1bd6763791a
Caio Brighenti <caio.brig@gmail.com>
2019-06-18 10:02:52 -0300

    Load rest of FakeNewsNet dataset

--- loaddata.R
+++ loaddata.R
@@ -11,5 +11,8 @@ survey_LIAR<-train[sample(nrow(train), 10), ]
 
 ## load FakeNewsNet dataset
 ffn_fake<-read.csv(file="FakeNewsNet/dataset/politifact_fake.CSV",header = FALSE)
+ffn_true<-read.csv(file="FakeNewsNet/dataset/politifact_real.CSV",header = FALSE)
+gossip_fake<-read.csv(file="FakeNewsNet/dataset/gossipcop_fake.CSV",header = FALSE)
+gossip_true<-read.csv(file="FakeNewsNet/dataset/gossipcop_real.CSV",header = FALSE)
 head(ffn_fake)
 survey_ffn<-ffn_fake[sample(nrow(ffn_fake), 10), ]
