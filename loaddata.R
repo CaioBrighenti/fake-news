048288d0d7f3b3fecaa0bd975d4d5351793f222d
Caio Brighenti <caio.brig@gmail.com>
2019-06-22 01:00:10 -0300

    begin parsing LIAR into fasttext format

--- loaddata.R
+++ loaddata.R
@@ -5,8 +5,33 @@ names(train)<-header
 head(train)
 # grab 10 random observations
 survey_LIAR<-train[sample(nrow(train), 10), ]
+# remove garbage observations
+train<-train[-which(as.character(train$label)=="brian-kemp"),]
+train<-train[-which(as.character(train$label)=="barack-obama"),]
+train<-train[-which(as.character(train$label)=="bill-mccollum"),]
+train<-train[-which(as.character(train$label)=="doug-macginnitie"),]
+# hack to get rid of bad levels
+train$label<-as.character(train$label)
+train$label<-as.factor(train$label)
+# reorder levels
+labels<-c("pants-fire","false","barely-true","half-true","mostly-true","true")
+for (num in 6:1) {
+  train$label <- relevel(train$label,labels[num])
+}
+levels(train$label)
+train$label<-unclass(train$label)
+# hack to get reset levels
+train$label<-as.numeric(train$label)
+train$label<-as.factor(train$label)
 
-
+## write LIAR to output
+file.create("liar_dataset/train.txt")
+#out<-file("liar_dataset/train.txt")
+for (i in 1:nrow(train)) {
+  line<-paste("__label__", as.character(train$label[i]), " ",train$statement[i], sep="")
+  write(line,file="liar_dataset/train.txt",append=TRUE)
+}
+#close(out)
 
 
 ## load FakeNewsNet dataset
@@ -18,3 +43,4 @@ head(ffn_fake)
 survey_ffn<-ffn_fake[sample(nrow(ffn_fake), 10), ]
 survey_gossip<-gossip_fake[sample(nrow(gossip_fake), 10), ]
   
+
