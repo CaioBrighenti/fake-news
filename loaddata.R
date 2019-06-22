6177a3b4e601adf07f647b51649bd71ad5fe3fd9
Caio Brighenti <caio.brig@gmail.com>
2019-06-21 21:07:55 -0300

    Remove fastText submodule

--- loaddata.R
+++ loaddata.R
@@ -16,3 +16,5 @@ gossip_fake<-read.csv(file="FakeNewsNet/dataset/gossipcop_fake.CSV",header = FAL
 gossip_true<-read.csv(file="FakeNewsNet/dataset/gossipcop_real.CSV",header = FALSE)
 head(ffn_fake)
 survey_ffn<-ffn_fake[sample(nrow(ffn_fake), 10), ]
+survey_gossip<-gossip_fake[sample(nrow(gossip_fake), 10), ]
+  
