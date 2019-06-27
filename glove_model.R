aeb4cb9e1fa63e9736ccc540ac8a5973b2237688
caiobrighenti <caio.brig@gmail.com>
2019-06-27 16:04:58 -0400

    Add TF-IDF weighting

--- glove_model.R
+++ glove_model.R
@@ -13,9 +13,9 @@ test <- loadLIARTest()
 ############## CREATE WORD EMBEDDINGS ############## 
 # Create iterator over tokens
 tokens <- train$statement %>%
-            lemmatize_strings %>%
-            tolower %>%
-            word_tokenizer
+  lemmatize_strings %>%
+  tolower %>%
+  word_tokenizer
 
 # Create vocabulary. Terms will be unigrams (simple words).
 ## remove stopwords
@@ -30,35 +30,41 @@ vectorizer <- vocab_vectorizer(vocab)
 # use window of 5 for context words
 tcm <- create_tcm(it, vectorizer, skip_grams_window = 5L)
 glove = GlobalVectors$new(word_vectors_size = 50, vocabulary = vocab, x_max = 10)
-word_vectors<-glove$fit_transform(tcm, n_iter = 20)
+word_vectors<-glove$fit_transform(tcm, n_iter = 35)
+
+############## GET TFIDF DTM ############## 
+dtm_train = create_dtm(it, vectorizer)
+tfidf = TfIdf$new()
+dtm_tfidf = tfidf$fit_transform(dtm_train)
 
 ############## CREATE DOC VECTORS############## 
 ## create doc vectors for train data
 train_dvec <- docVector(tokens, vocab, word_vectors)
 mode(train_dvec) = "numeric"
 dat_train<-data.frame(label=as.factor(unclass(train$label)),train_dvec)
-head(dat_train)
+
+
 
 ## create doc vectors for test data
-test_tokens <- space_tokenizer(test$statement)  
 test_dvec <- docVector(test_tokens, vocab, word_vectors)
 mode(test_dvec) = "numeric"
 dat_test<-data.frame(label=as.factor(unclass(test$label)),test_dvec)
-head(dat_test)
-
 
 ############## FIT MODELS ############## 
 ## fit ordinal logistic model
 library(MASS)
 mod.polr<-polr(as.factor(label)~.,data=dat_train,Hess=TRUE)
-summary(mod.polr)
+#summary(mod.polr)
+calcAccuracy(mod.polr, dat_train)
+calcAccuracy(mod.polr, dat_test)
 
-calcAccuracy(mod.polr, dat_train, dat_train$label)
-## test accuracy
-calcAccuracy(mod.polr, dat_test, dat_test$label)
+## SVM
+library(e1071)
+mod.svm<-svm(as.factor(label)~.,data=dat_train, kernel="linear", cost=10, scale=FALSE)
+#tune.out<-tune(svm,as.factor(label)~.,data=dat_train,kernel="linear", ranges=list(cost=c(0.001,0.01,0.1,1,5,10,100)))
+calcAccuracy(mod.svm, dat_test)
 
-
-
+############## HELPER FUNCTIONS ############## 
 docVector <- function(tokens,vocab,word_vectors){
   ## create doc vectors for train data
   doc_vectors<-matrix(0, nrow=length(tokens), ncol=50)
@@ -79,13 +85,53 @@ docVector <- function(tokens,vocab,word_vectors){
     # add document to matrix
     doc_vectors[i,]<-doc_vec
   }
-
+  
   return(doc_vectors)
 }
 
-calcAccuracy <- function(mod,new_data,true_labels) {
+docVectorWeighted <- function(tokens,vocab,word_vectors,weights){
+  ## create doc vectors for train data
+  doc_vectors<-matrix(0, nrow=length(tokens), ncol=50)
+  ## loop through each document
+  for (i in 1:length(tokens)) {
+    # grab words in doc
+    words<-unlist(tokens[i])
+    # remove pruned words and stopwords
+    words<-words[which(words %in% vocab$term)]
+    if (length(words)==0) {
+      doc_vectors[i,]<-rep(0,50)
+      next
+    }
+    # get word vectors to calc doc vec
+    word_vecs<-word_vectors[words, ]
+    # tfidf weighing
+    doc_weights <- weights[i,][which(weights[i,]!=0)]
+    ## should vectorize but for now loop
+    if (length(words)>1){
+      for (i in 1:length(words)) {
+        word<-words[i]
+        word_vecs[i,] <- word_vecs[i,] * doc_weights[word]
+      }
+    } else {
+      word_vecs <- word_vecs * doc_weights
+    }
+    # get document vector
+    if (length(words)>1){
+      doc_vec <- colMeans(word_vecs)
+    } else if (length(words) == 1){
+      doc_vec <- word_vecs
+    }
+    # add document to matrix
+    doc_vectors[i,]<-doc_vec
+  }
+  return(doc_vectors)
+}
+
+calcAccuracy <- function(mod,new_data,adj=0) {
+  true_labels <- new_data$label
   pred <- predict(mod, newdata = new_data, type="class")
-  acc <- mean(pred == true_labels)
+  dist <- abs(as.numeric(pred)-as.numeric(true_labels))
+  acc <-  mean(dist <= adj)
   return(acc)
 }
 
