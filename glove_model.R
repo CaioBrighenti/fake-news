96d60183722166c1da82a4e50efffdf33abe5f84
caiobrighenti <caio.brig@gmail.com>
2019-06-27 13:26:12 -0400

    Add preprocessing to GloVe model

--- glove_model.R
+++ glove_model.R
@@ -1,17 +1,30 @@
+############## LOAD ############## 
+## load in libs
+library("text2vec")
+library("tidytext")
+library("stopwords")
+library("dplyr")
+library("textstem")
 # load in data
 source("loaddata.R")
 train <- loadLIARTrain()
 test <- loadLIARTest()
 
-
-## create word embeddings
-library("text2vec")
+############## CREATE WORD EMBEDDINGS ############## 
 # Create iterator over tokens
-tokens <- space_tokenizer(train$statement)
+tokens <- train$statement %>%
+            lemmatize_strings %>%
+            tolower %>%
+            word_tokenizer
+
 # Create vocabulary. Terms will be unigrams (simple words).
-it = itoken(tokens, progressbar = FALSE)
-vocab <- create_vocabulary(it) 
+## remove stopwords
+it = itoken(iterable = tokens,
+            progressbar = FALSE)
+vocab <- create_vocabulary(it,stopwords = stopwords('en')) 
+## remove infrequent words and stem words
 vocab <- prune_vocabulary(vocab, term_count_min = 5L)
+#vocab$term <- lemmatize_strings(vocab$term)
 # Use our filtered vocabulary
 vectorizer <- vocab_vectorizer(vocab)
 # use window of 5 for context words
@@ -19,30 +32,28 @@ tcm <- create_tcm(it, vectorizer, skip_grams_window = 5L)
 glove = GlobalVectors$new(word_vectors_size = 50, vocabulary = vocab, x_max = 10)
 word_vectors<-glove$fit_transform(tcm, n_iter = 20)
 
+############## CREATE DOC VECTORS############## 
 ## create doc vectors for train data
 train_dvec <- docVector(tokens, vocab, word_vectors)
 mode(train_dvec) = "numeric"
 dat_train<-data.frame(label=as.factor(unclass(train$label)),train_dvec)
 head(dat_train)
 
-## fit ordinal logistic model
-#library(MASS)
-mod.polr<-polr(as.factor(label)~.,data=dat_train,Hess=TRUE)
-summary(mod.polr)
-
-calcAccuracy(mod.polr, dat_train, dat_train$label)
-
-
-
-
 ## create doc vectors for test data
 test_tokens <- space_tokenizer(test$statement)  
-## create doc vectors for train data
 test_dvec <- docVector(test_tokens, vocab, word_vectors)
 mode(test_dvec) = "numeric"
 dat_test<-data.frame(label=as.factor(unclass(test$label)),test_dvec)
 head(dat_test)
 
+
+############## FIT MODELS ############## 
+## fit ordinal logistic model
+library(MASS)
+mod.polr<-polr(as.factor(label)~.,data=dat_train,Hess=TRUE)
+summary(mod.polr)
+
+calcAccuracy(mod.polr, dat_train, dat_train$label)
 ## test accuracy
 calcAccuracy(mod.polr, dat_test, dat_test$label)
 
@@ -55,7 +66,7 @@ docVector <- function(tokens,vocab,word_vectors){
   for (i in 1:length(tokens)) {
     # grab words in doc
     words<-unlist(tokens[i])
-    # remove pruned words
+    # remove pruned words and stopwords
     words<-words[which(words %in% vocab$term)]
     # calculate document mean
     if (length(words)>1){
