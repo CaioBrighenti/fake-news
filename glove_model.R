0d2c2eadc3ded3470c9ea6d2d58e898f26730a83
Caio Brighenti <caio.brig@gmail.com>
2019-06-27 09:31:44 -0400

    Refactor document vector creation

--- glove_model.R
+++ glove_model.R
@@ -19,50 +19,58 @@ tcm <- create_tcm(it, vectorizer, skip_grams_window = 5L)
 glove = GlobalVectors$new(word_vectors_size = 50, vocabulary = vocab, x_max = 10)
 word_vectors<-glove$fit_transform(tcm, n_iter = 20)
 
-## word similarity
-x <- word_vectors["Trump", , drop = FALSE]
-cos_sim = sim2(x = word_vectors, y = x, method = "cosine", norm = "l2")
-head(sort(cos_sim[,1], decreasing = TRUE), 5)
-
-
-## create doc vectors
-doc_vectors<-matrix(0, nrow=length(tokens), ncol=50)
-## loop through each document
-for (i in 1:length(tokens)) {
-  # grab words in doc
-  words<-unlist(tokens[i])
-  # remove pruned words
-  words<-words[which(words %in% vocab$term)]
-  # calculate document mean
-  if (length(words)>1){
-    doc_vec <- colMeans(word_vectors[words, ])
-  } else if (length(words) == 1){
-    doc_vec <- words
-  } else {
-    doc_vec <- rep(0,50)
-  }
-  # add document to matrix
-  doc_vectors[i,]<-doc_vec
-}
-
-dat<-data.frame(label=as.factor(unclass(train$label)),doc_vectors)
-head(dat)
-
-## make sure predictors are numberic
-### shouldn't be necessary but I GUESS THAT'S OK
-for (idx in 2:51) {
-  dat[,idx]<-as.numeric(type.convert(dat[,idx]))
-}
-
+## create doc vectors for train data
+train_dvec <- docVector(tokens, vocab, word_vectors)
+mode(train_dvec) = "numeric"
+dat_train<-data.frame(label=as.factor(unclass(train$label)),train_dvec)
+head(dat_train)
 
 ## fit ordinal logistic model
-library(MASS)
-#mod<-lm(as.numeric(label)~.,data=dat)
-#summary(mod)
-mod.polr<-polr(label~.,data=dat,Hess=TRUE)
+#library(MASS)
+mod.polr<-polr(as.factor(label)~.,data=dat_train,Hess=TRUE)
 summary(mod.polr)
 
-calcAccuracy(mod.polr, dat, dat$label)
+calcAccuracy(mod.polr, dat_train, dat_train$label)
+
+
+
+
+## create doc vectors for test data
+test_tokens <- space_tokenizer(test$statement)  
+## create doc vectors for train data
+test_dvec <- docVector(test_tokens, vocab, word_vectors)
+mode(test_dvec) = "numeric"
+dat_test<-data.frame(label=as.factor(unclass(test$label)),test_dvec)
+head(dat_test)
+
+## test accuracy
+calcAccuracy(mod.polr, dat_test, dat_test$label)
+
+
+
+docVector <- function(tokens,vocab,word_vectors){
+  ## create doc vectors for train data
+  doc_vectors<-matrix(0, nrow=length(tokens), ncol=50)
+  ## loop through each document
+  for (i in 1:length(tokens)) {
+    # grab words in doc
+    words<-unlist(tokens[i])
+    # remove pruned words
+    words<-words[which(words %in% vocab$term)]
+    # calculate document mean
+    if (length(words)>1){
+      doc_vec <- colMeans(word_vectors[words, ])
+    } else if (length(words) == 1){
+      doc_vec <- word_vectors[words, ]
+    } else {
+      doc_vec <- rep(0,50)
+    }
+    # add document to matrix
+    doc_vectors[i,]<-doc_vec
+  }
+
+  return(doc_vectors)
+}
 
 calcAccuracy <- function(mod,new_data,true_labels) {
   pred <- predict(mod, newdata = new_data, type="class")
