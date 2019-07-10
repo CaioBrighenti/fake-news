300a749efa984dbce6eeb136119290bfd038be40
Caio Brighenti <caio.brig@gmail.com>
2019-07-10 09:37:51 -0400

    Add and analyze partial survey data

---
+++ survey.R
@@ -0,0 +1,44 @@
+# libraries
+library(dplyr)
+library(tidyverse)
+
+survey_claims<-read.csv(file="survey/survey_claims.csv",header = TRUE)
+survey_claims <- as_tibble(survey_claims)
+
+
+## analysize truth ratings
+survey_truth <- survey_claims %>% 
+  dplyr::select(ID, label, `2`=FALSE., `3`=btrue, `4`=htrue, `5`=mtrue, `6`=TRUE.) %>%
+  mutate(total = `2` + `3` + `4` + `5` + `6`)
+correct <- c(14,13,22,13,15,15,3,18,19,29)
+adjacent <- c(14+12,27+15,13+16,15,7,8+12,7,14,9+20,7+18)
+survey_truth <- survey_truth %>% 
+  mutate(corr = correct, adjacent = adjacent + corr, acc = corr / total, adj = adjacent / total) %>%
+  mutate(mean_truth = (2*`2`+3*`3`+4*`4`+5*`5`+6*`6`)/total)
+## mean accuracy
+survey_truth %>% 
+  summarise(mean_acc = mean(acc), mean_adj = mean(adj))
+survey_truth %>% 
+  group_by(label) %>%
+  summarise(mean_acc = mean(acc), mean_adj = mean(adj))
+
+
+## analyze types
+survey_types <- survey_claims %>%
+  dplyr::select(ID, label, omits, deceptive, lie, projection, vague, satire) %>%
+  mutate(mean.truth = survey_truth$mean_truth)
+
+
+## analyze justifications
+survey_just <- survey_claims %>%
+  dplyr::select(ID, label, speaker, knowledge, words, grammar, intuition, other)
+
+# means
+survey_just %>%
+  summarise(speaker=mean(speaker), knowledge=mean(knowledge), words=mean(words), 
+            grammar=mean(grammar), intuition=mean(intuition), other=mean(other))
+
+survey_just %>%
+  group_by(label) %>%
+  summarise(speaker=mean(speaker), knowledge=mean(knowledge), words=mean(words), 
+            grammar=mean(grammar), intuition=mean(intuition), other=mean(other))
---
+++ survey/truth_dictionary.py
@@ -0,0 +1,7 @@
+import os
+import re
+os.getcwd()
+#os.chdir('C:/Users/Caio/repos/nba-models')
+
+f = open('workfile', 'w')
+f.read()
