3ca6ac763a183ff78d9bed698ee0bf0f8a33d63d
Caio Brighenti <caio.brig@gmail.com>
2019-07-25 14:34:13 -0400

    Update FNN encoding to ASCII from UTF-8

--- processFNN.py
+++ processFNN.py
@@ -3,6 +3,7 @@ import json
 import re
 import csv
 
+
 os.getcwd()
 os.chdir('C:/Users/Caio Brighenti/Documents/repositories/fake-news')
 
@@ -23,10 +24,10 @@ for dirName, subdirList, fileList in os.walk(fnn_path):
             datastore = json.load(f)
             ## grab needed parts
             title = datastore["title"]
-            #title = re.sub('[^A-Za-z0-9]+', ' ', title)
+            title = re.sub('[^A-Za-z0-9]+', ' ', title)
             text = datastore["text"]
             text = re.sub(r'\n',' ', text)
-            #text = re.sub('[^A-Za-z0-9]+', ' ', text)
+            text = re.sub('[^A-Za-z0-9]+', ' ', text)
             ## grab article ID
             ID = dirName.split("\\")[-1]
             ## grab truth label
@@ -34,4 +35,4 @@ for dirName, subdirList, fileList in os.walk(fnn_path):
             ## append to CSV
             with open('FakeNewsNet/dataset/fnn_data.tsv', 'a') as f:
                 tsv_writer = csv.writer(f, delimiter='\t')
-                tsv_writer.writerow([ID,label,title.encode("utf-8"), text.encode("utf-8")])
+                tsv_writer.writerow([ID,label,title.encode("ascii"), text.encode("ascii")])
