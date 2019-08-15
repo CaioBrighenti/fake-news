4ab870b265a6c1ac50f7bcc044b99134289c2b23
Caio Brighenti <caio.brig@gmail.com>
2019-08-15 15:00:07 -0400

    improve accuracy calculation and add method to calculate cutoff based ROC curve

--- helpers.R
+++ helpers.R
@@ -1,21 +1,25 @@
 library(caret)
+library(progress)
 ############## HELPER FUNCTIONS ############## 
-calcAccuracyLR <- function(mod,new_data,adj=0, true_labels = NULL) {
+calcAccuracyLR <- function(mod,new_data,adj=0, true_labels = NULL, cutoff = 0.5) {
   if (is.null(true_labels)){
-    true_labels <- new_data$`_label`
-    if (is.null(true_labels)){true_labels <- new_data$label}
+    if('_label' %in% names(new_data)) {
+      true_labels <- new_data$`_label`
+    } else {
+      true_labels <- new_data$label
+    }
   }
-  pred <- predict(mod, new_data)
-  class <- as.numeric(pred > 0.5)
-  dist <- abs(as.numeric(class)-(as.numeric(true_labels)-1))
-  acc <-  mean(dist <= adj)
+  levels(true_labels) <- c(0,1)
+  pred <- predict(mod, new_data, type = "response")
+  class <- factor(as.numeric(pred > cutoff), levels=c(0,1))
+  acc <-  mean(class == true_labels)
   ## [1] = sensitivity/recall, [2] = specificity, [5] = precision, [7] = F1
-  cm <- confusionMatrix(as.factor(class), true_labels, positive="1")
+  cm <- confusionMatrix(table(class, true_labels), positive="1")
   # calculate F1
   stats <- tibble(accuracy = acc, sensitivity = cm$byClass[1],
                   specificity = cm$byClass[2], precision = cm$byClass[5],
                   F1 = cm$byClass[7], c_matrix = cm$table)
-  print(stats)
+  #print(stats)
   return(stats)
 }
 
@@ -44,8 +48,8 @@ plotPredictions <- function(mods,dat_test){
 
 getVarRanks <- function(data) {
   ## get rid of unnecessary columns
-  if('title' %in% names(data)){data <- data %>% select(-title)}
-  if('text' %in% names(data)){data <- data %>% select(-text)}
+  if('title' %in% names(data)){data <- data %>% dplyr::select(-title)}
+  if('text' %in% names(data)){data <- data %>% dplyr::select(-text)}
   
   aovs <- data %>%
     dplyr::select(-ID, -label) %>%
@@ -58,10 +62,41 @@ getVarRanks <- function(data) {
     dplyr::select(-ID, -label) %>%
     filterVarImp(.,data$label) %>%
     mutate(var = row.names(.), max_varimp = pmax(fake,real), varimp_rank = rank(-max_varimp)) %>%
-    select(var, max_varimp, varimp_rank) %>%
+    dplyr::select(var, max_varimp, varimp_rank) %>%
     left_join(aovs, by = 'var') %>%
     mutate(p_rank = rank(p_val), avg_rank = (varimp_rank + p_rank) / 2) %>%
     arrange(avg_rank)
   
   return(ranks)
 }
+
+getROC <- function(mod, data){
+  colgate_ter <- c("#64A50A", "#F0AA00","#0096C8", "#005F46","#FF6914","#004682")
+  roc_tib <- tibble(
+    cutoff = seq(0,1,by=0.01),
+    sensitivity = rep(0,length(cutoff)),
+    specificity = rep(0,length(cutoff)),
+    accuracy = rep(0,length(cutoff))
+  )
+  pb <- progress_bar$new(format = "[:bar] :current/:total (:percent)", total = nrow(roc_tib))
+  for (idx in 1:nrow(roc_tib)) {
+    pb$tick()
+    cutoff <- roc_tib[idx,]$cutoff
+    acc_table <- calcAccuracyLR(mod, data, cutoff = cutoff)
+    roc_tib[idx,]$sensitivity <- acc_table$sensitivity[1]
+    roc_tib[idx,]$specificity <- acc_table$specificity[1]
+    roc_tib[idx,]$accuracy <- acc_table$accuracy[1]
+  }
+  p<-roc_tib %>%
+    ggplot(aes(x=1-specificity, y=sensitivity, color=cutoff, size = accuracy)) +
+    geom_point() +
+    scale_color_gradient(low=colgate_ter[2],high=colgate_ter[3])
+  print(p)
+  
+  best_cut <- roc_tib %>%
+    mutate(crit1 = sensitivity + specificity, crit2 = abs(specificity - sensitivity)) %>%
+    mutate(acc_rank = rank(-accuracy), crit1 = rank(-crit1), crit2 = rank(crit2)) %>%
+    filter(acc_rank == 1 | crit1 == 1 | crit2 == 1)
+  
+  return(best_cut)
+}    
